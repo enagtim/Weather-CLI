770cb61663ea6129376b0e826912415ff423e579
Nikita Kaberov <enagtim@mail.ru>
2024-10-20 10:50:30 +0300

    Реализовано отражение аргументов командной строки

---
+++ weather.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { getArgs } from "./helpers/arg.js";
+const initCLI = () => {
+  const args = getArgs(process.argv);
+  console.log(args);
+  if (args.h) {
+  }
+  if (args.s) {
+  }
+  if (args.t) {
+  }
+};
+initCLI();
