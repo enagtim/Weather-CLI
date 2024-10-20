1aed3bdf196a98a5b7e8538153472d2f36e2a9c6
Nikita Kaberov <enagtim@mail.ru>
2024-10-20 12:55:07 +0300

    Добавлена работа с файловой системой

--- weather.js
+++ weather.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/arg.js";
-import { printHelp } from "./services/log.service.js";
+import { printHelp, printSuccess, printError } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Токен сохранен");
+  } catch (e) {
+    printError(e.message);
+  }
+};
 const initCLI = () => {
   const args = getArgs(process.argv);
   if (args.h) {
@@ -9,6 +18,7 @@ const initCLI = () => {
   if (args.s) {
   }
   if (args.t) {
+    return saveToken(args.t);
   }
 };
 initCLI();
