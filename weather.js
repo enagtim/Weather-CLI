27da1baacaa2675159d438ff0ad1e5923e61de2b
Nikita Kaberov <enagtim@mail.ru>
2024-10-20 11:13:46 +0300

    Реализован вывод в консоль

--- weather.js
+++ weather.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/arg.js";
+import { printHelp } from "./services/log.service.js";
 const initCLI = () => {
   const args = getArgs(process.argv);
-  console.log(args);
   if (args.h) {
+    printHelp();
   }
   if (args.s) {
   }
